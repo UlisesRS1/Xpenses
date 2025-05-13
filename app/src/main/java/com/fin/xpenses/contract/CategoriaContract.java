1a5760c36b0e5755e5ff03af4ef2f2b88c5e0a35
UlisesRS1 <rankulimarcus@gmail.com>
2025-05-13 13:28:41 -0600

    Contracts 3/5

--- app/src/main/java/com/fin/xpenses/contract/CategoriaContract.java
+++ app/src/main/java/com/fin/xpenses/contract/CategoriaContract.java
@@ -5,11 +5,11 @@ import android.provider.BaseColumns;
 public class CategoriaContract {
     public static class CategoriasEntry implements BaseColumns {
         /* Nombre de la tabla */
-        private static final String TABLE_NAME = "Categorias";
+        public static final String TABLE_NAME = "Categorias";
 
         /* Columnas de la tabla */
-        private static final String ID_CATEGORIAS = "id_categorias";
-        private static final String CATEGORIA = "categoria";
-        private static final String ID_TIPO_CATEGORIA = "id_tipo_categoria";
+        public static final String ID_CATEGORIAS = "id_categorias";
+        public static final String CATEGORIA = "categoria";
+        public static final String ID_TIPO_CATEGORIA = "id_tipo_categoria";
     }
 }
