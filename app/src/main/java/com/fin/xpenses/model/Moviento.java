332cae89fd5d0aa7a7bbcc7ae9443ac7181ce35d
JairoMtz28 <alvaradomartinezjairojafet@gmail.com>
2025-05-14 07:59:50 -0600

    Model 5/5

--- app/src/main/java/com/fin/xpenses/model/Moviento.java
+++ app/src/main/java/com/fin/xpenses/model/Moviento.java
@@ -1,4 +1,13 @@
 package com.fin.xpenses.model;
 
+import lombok.Data;
+
+@Data
 public class Moviento {
+    private int idMovimiento;
+    private double monto;
+    private String fecha;
+    private Categoria idCategoria;
+    private boolean esFuturo;
+    private String fechaRegistro;
 }
--- app/src/main/java/com/fin/xpenses/model/Recordatorio.java
+++ app/src/main/java/com/fin/xpenses/model/Recordatorio.java
@@ -1,4 +1,11 @@
 package com.fin.xpenses.model;
 
+import lombok.Data;
+
+@Data
 public class Recordatorio {
+    private int idRecordatorio;
+    private Moviento idMovimiento;
+    private String mensaje;
+    private String fechaAlarma;
 }
