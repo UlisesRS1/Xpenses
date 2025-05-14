15ac62b237d2f48bf787b247d3a555fad4d41828
Angel <angelnoecg@gmail.com>
2025-05-14 08:15:39 -0600

    repository 3/3

--- app/src/main/java/com/fin/xpenses/repository/IRecordatorioRepository.java
+++ app/src/main/java/com/fin/xpenses/repository/IRecordatorioRepository.java
@@ -1,4 +1,14 @@
 package com.fin.xpenses.repository;
 
+
+import com.fin.xpenses.model.Recordatorio;
+
+import java.util.List;
+
 public interface IRecordatorioRepository {
+    boolean agregarRecordatorio(Recordatorio recordatorio);
+    boolean eliminarRecordatorio(int idRecordatorio);
+    boolean actualizarRecordatorio(Recordatorio recordatorio);
+    Recordatorio obtenerRecordatorio(int idRecordatorio);
+    List<Recordatorio> obtenerTodosLosRecordatorios();
 }
--- app/src/main/java/com/fin/xpenses/repository/IRepeticionRepository.java
+++ app/src/main/java/com/fin/xpenses/repository/IRepeticionRepository.java
@@ -1,4 +1,13 @@
 package com.fin.xpenses.repository;
 
+import com.fin.xpenses.model.Repeticion;
+
+import java.util.List;
+
 public interface IRepeticionRepository {
+    boolean agregarRepeticion(Repeticion repeticion);
+    boolean eliminarRepeticion(int idRepeticion);
+    boolean actualizarRepeticion(Repeticion repeticion);
+    Repeticion obtenerRepeticion(int idRepeticion);
+    List<Repeticion> obtenerTodasLasRepeticiones();
 }
