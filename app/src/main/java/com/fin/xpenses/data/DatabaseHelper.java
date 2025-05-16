d56b7b2e65364c35d76174bae0d2cea559667c81
CesarE301 <cesar12345chuy11@gmail.com>
2025-05-16 09:12:12 -0600

    Create tables

--- app/src/main/java/com/fin/xpenses/data/DatabaseHelper.java
+++ app/src/main/java/com/fin/xpenses/data/DatabaseHelper.java
@@ -6,6 +6,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.fin.xpenses.contract.CategoriaContract;
 import com.fin.xpenses.contract.MovimientoContract;
+import com.fin.xpenses.contract.RecordatorioContract;
+import com.fin.xpenses.contract.RepeticionContract;
 import com.fin.xpenses.contract.TipoCategoriaContract;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -23,15 +25,33 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             CategoriaContract.CategoriasEntry.TABLE_NAME +"(" + CategoriaContract.CategoriasEntry.ID_CATEGORIAS + "));";
     private static final String SQL_CREATE_CATEGORIA = "CREATE TABLE IF NOT EXISTS "+
             CategoriaContract.CategoriasEntry.TABLE_NAME +" ( "+
-            CategoriaContract.CategoriasEntry.ID_CATEGORIAS + "INTEGER PRIMARY KEY AUTOINCREMENT, "+
-            CategoriaContract.CategoriasEntry.CATEGORIA + "TEXT NOT NULL, "+
-            CategoriaContract.CategoriasEntry.ID_TIPO_CATEGORIA + "INTEGER NOT NULL, "+
+            CategoriaContract.CategoriasEntry.ID_CATEGORIAS + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+            CategoriaContract.CategoriasEntry.CATEGORIA + " TEXT NOT NULL, "+
+            CategoriaContract.CategoriasEntry.ID_TIPO_CATEGORIA + " INTEGER NOT NULL, "+
             "FOREIGN KEY (" + CategoriaContract.CategoriasEntry.ID_TIPO_CATEGORIA + ") REFERENCES "+
             TipoCategoriaContract.TipoCategoriaEntry.TABLE_NAME+"("+ TipoCategoriaContract.TipoCategoriaEntry.ID_TIPO_CATEGORIA+ "));";
     private static final String SQL_CREATE_TIPOCATEGORIA = "CREATE TABLE IF NOT EXISTS "+
             TipoCategoriaContract.TipoCategoriaEntry.TABLE_NAME +" ( "+
-            TipoCategoriaContract.TipoCategoriaEntry.ID_TIPO_CATEGORIA +"INTEGER PRIMARY KEY AUTOINCREMENT, "+
-            TipoCategoriaContract.TipoCategoriaEntry.TIPO + "TEXT NOT NULL, "+ ");";
+            TipoCategoriaContract.TipoCategoriaEntry.ID_TIPO_CATEGORIA +" INTEGER PRIMARY KEY AUTOINCREMENT, "+
+            TipoCategoriaContract.TipoCategoriaEntry.TIPO + " TEXT NOT NULL, "+ ");";
+    private static final String SQL_CREATE_RECORDATORIO = "CREATE TABLE IF NOT EXISTS "+
+            RecordatorioContract.RecordatorioEntry.TABLE_NAME +" ( "+
+            RecordatorioContract.RecordatorioEntry.ID_RECORDATORIO +" INTEGER PRIMARY KEY AUTOINCREMENT, "+
+            RecordatorioContract.RecordatorioEntry.ID_MOVIMIENTO + " INTEGER NOT NULL, "+
+            RecordatorioContract.RecordatorioEntry.MENSAJE + " TEXT NOT NULL, "+
+            RecordatorioContract.RecordatorioEntry.FECHA_ALARMA + " TEXT NOT NULL, "+
+            "FOREIGN KEY (" + RecordatorioContract.RecordatorioEntry.ID_MOVIMIENTO + ") REFERENCES "+
+            MovimientoContract.MovimientoEntry.TABLE_NAME+"("+ MovimientoContract.MovimientoEntry.ID_MOVIMIENTO+ "));";
+    private static final String SQL_CREATE_REPETICIONES = "CREATE TABLE IF NOT EXISTS "+
+            RepeticionContract.RepeticionesEntry.TABLE_NAME +" ( "+
+            RepeticionContract.RepeticionesEntry.ID_REPETICION +" INTEGER PRIMARY KEY AUTOINCREMENT, "+
+            RepeticionContract.RepeticionesEntry.ID_CATEGORIA + " INTEGER NOT NULL, "+
+            RepeticionContract.RepeticionesEntry.MONTO + " REAL NOT NULL, "+
+            RepeticionContract.RepeticionesEntry.FECHA_INICIO + " TEXT NOT NULL, "+
+            RepeticionContract.RepeticionesEntry.FECHA_TERMINO + " TEXT NOT NULL, "+
+            RepeticionContract.RepeticionesEntry.DIAS_ESPECIFICOS + " TEXT NOT NULL, "+
+            "FOREIGN KEY (" + RepeticionContract.RepeticionesEntry.ID_CATEGORIA + ") REFERENCES "+
+            CategoriaContract.CategoriasEntry.TABLE_NAME+"("+ CategoriaContract.CategoriasEntry.ID_CATEGORIAS+ "));";
 
 
 
@@ -42,7 +62,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_TIPOCATEGORIA);
+        db.execSQL(SQL_CREATE_CATEGORIA);
+        db.execSQL(SQL_CREATE_MOVIMIENTOS);
+        db.execSQL(SQL_CREATE_RECORDATORIO);
+        db.execSQL(SQL_CREATE_REPETICIONES);
     }
 
     @Override
