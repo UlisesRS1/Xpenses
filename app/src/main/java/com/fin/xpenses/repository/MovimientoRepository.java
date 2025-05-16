72cf7a232987eab90afaf0f9c74ee3ab34af1380
Angel <angelnoecg@gmail.com>
2025-05-16 09:13:12 -0600

    metodos agregar en repository

--- app/src/main/java/com/fin/xpenses/repository/MovimientoRepository.java
+++ app/src/main/java/com/fin/xpenses/repository/MovimientoRepository.java
@@ -1,4 +1,59 @@
 package com.fin.xpenses.repository;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.fin.xpenses.contract.MovimientoContract;
+import com.fin.xpenses.data.DatabaseHelper;
+import com.fin.xpenses.model.Movimiento;
+
+import java.util.Collections;
+import java.util.List;
+
 public class MovimientoRepository implements IMovimientoRepository{
+    private DatabaseHelper databaseHelper;
+    public MovimientoRepository(DatabaseHelper databaseHelper) {
+        this.databaseHelper = databaseHelper;
+    }
+    @Override
+    public boolean agregarMovimiento(Movimiento movimiento) {
+        SQLiteDatabase db;
+        ContentValues values;
+        try{
+            db = databaseHelper.getWritableDatabase();
+            values = new ContentValues();
+            values.put(MovimientoContract.MovimientoEntry.ID_MOVIMIENTO, movimiento.getIdMovimiento());
+            values.put(MovimientoContract.MovimientoEntry.MONTO, movimiento.getMonto());
+            values.put(MovimientoContract.MovimientoEntry.FECHA, movimiento.getFecha());
+            values.put(MovimientoContract.MovimientoEntry.ID_CATEGORIAS, movimiento.getIdCategoria().getIdCategoria());
+            values.put(MovimientoContract.MovimientoEntry.ES_FUTURO, movimiento.isEsFuturo());
+            values.put(MovimientoContract.MovimientoEntry.FECHA_REGISTRO, movimiento.getFechaRegistro());
+            long insert = db.insert(MovimientoContract.MovimientoEntry.TABLE_NAME, null, values);
+            return insert != -1;
+        }catch (Exception e){
+            Log.e("Error", e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean eliminarMovimiento(int idMovimiento) {
+        return false;
+    }
+
+    @Override
+    public boolean actualizarMovimiento(Movimiento movimiento) {
+        return false;
+    }
+
+    @Override
+    public Movimiento obtenerMovimiento(int idMovimiento) {
+        return null;
+    }
+
+    @Override
+    public List<Movimiento> obtenerTodosLosMovimientos() {
+        return Collections.emptyList();
+    }
 }
--- app/src/main/java/com/fin/xpenses/repository/RepeticionRepository.java
+++ app/src/main/java/com/fin/xpenses/repository/RepeticionRepository.java
@@ -1,4 +1,59 @@
 package com.fin.xpenses.repository;
 
-public class RepeticionRepository {
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.fin.xpenses.contract.RepeticionContract;
+import com.fin.xpenses.data.DatabaseHelper;
+import com.fin.xpenses.model.Repeticion;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RepeticionRepository implements IRepeticionRepository{
+    private DatabaseHelper databaseHelper;
+    public RepeticionRepository(DatabaseHelper databaseHelper) {
+        this.databaseHelper = databaseHelper;
+    }
+    @Override
+    public boolean agregarRepeticion(Repeticion repeticion) {
+        SQLiteDatabase db;
+        ContentValues values;
+        try {
+            db = databaseHelper.getWritableDatabase();
+            values = new ContentValues();
+            values.put(RepeticionContract.RepeticionesEntry.ID_REPETICION, repeticion.getIdRepeticion());
+            values.put(RepeticionContract.RepeticionesEntry.ID_CATEGORIA, repeticion.getIdCategoria().getIdCategoria());
+            values.put(RepeticionContract.RepeticionesEntry.MONTO, repeticion.getMonto());
+            values.put(RepeticionContract.RepeticionesEntry.FECHA_INICIO, repeticion.getFechaInicia());
+            values.put(RepeticionContract.RepeticionesEntry.FECHA_TERMINO, repeticion.getFechaTermina());
+            values.put(RepeticionContract.RepeticionesEntry.DIAS_ESPECIFICOS, repeticion.getDiasEspecificos());
+            long insert = db.insert(RepeticionContract.RepeticionesEntry.TABLE_NAME, null, values);
+            return insert != -1;
+        }catch (Exception e){
+                Log.e("Error", e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean eliminarRepeticion(int idRepeticion) {
+        return false;
+    }
+
+    @Override
+    public boolean actualizarRepeticion(Repeticion repeticion) {
+        return false;
+    }
+
+    @Override
+    public Repeticion obtenerRepeticion(int idRepeticion) {
+        return null;
+    }
+
+    @Override
+    public List<Repeticion> obtenerTodasLasRepeticiones() {
+        return Collections.emptyList();
+    }
 }
