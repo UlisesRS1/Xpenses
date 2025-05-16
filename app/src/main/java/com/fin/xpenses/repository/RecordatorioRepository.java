76e343e64bc5b78123b36bbd7094c7c8c1d317ce
UlisesRS1 <rankulimarcus@gmail.com>
2025-05-16 09:02:06 -0600

    Add methods to RecordatorioRepository

--- app/src/main/java/com/fin/xpenses/repository/RecordatorioRepository.java
+++ app/src/main/java/com/fin/xpenses/repository/RecordatorioRepository.java
@@ -1,4 +1,41 @@
 package com.fin.xpenses.repository;
 
-public class RecordatorioRepository {
+import com.fin.xpenses.data.DatabaseHelper;
+import com.fin.xpenses.model.Recordatorio;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RecordatorioRepository implements IRecordatorioRepository{
+    private DatabaseHelper databaseHelper;
+
+    public RecordatorioRepository(DatabaseHelper databaseHelper) {
+        this.databaseHelper = databaseHelper;
+    }
+
+
+    @Override
+    public boolean agregarRecordatorio(Recordatorio recordatorio) {
+        return false;
+    }
+
+    @Override
+    public boolean eliminarRecordatorio(int idRecordatorio) {
+        return false;
+    }
+
+    @Override
+    public boolean actualizarRecordatorio(Recordatorio recordatorio) {
+        return false;
+    }
+
+    @Override
+    public Recordatorio obtenerRecordatorio(int idRecordatorio) {
+        return null;
+    }
+
+    @Override
+    public List<Recordatorio> obtenerTodosLosRecordatorios() {
+        return Collections.emptyList();
+    }
 }
