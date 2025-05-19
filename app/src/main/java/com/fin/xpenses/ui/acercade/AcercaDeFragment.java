68c017a44f895dfe8d757a6e957b02daf7da08a4
Angel <angelnoecg@gmail.com>
2025-05-19 13:38:11 -0600

    Diseño de la pagina acerca de

---
+++ app/src/main/java/com/fin/xpenses/ui/acercade/AcercaDeFragment.java
@@ -0,0 +1,29 @@
+package com.fin.xpenses.ui.acercade;
+
+import androidx.fragment.app.Fragment;
+
+//import com.fin.xpenses.databinding.FragmentSlideshowBinding;
+
+public class AcercaDeFragment extends Fragment {
+
+    //private FragmentSlideshowBinding binding;
+
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             ViewGroup container, Bundle savedInstanceState) {
+//        SlideshowViewModel slideshowViewModel =
+//                new ViewModelProvider(this).get(SlideshowViewModel.class);
+//
+//        //binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+//        //View root = binding.getRoot();
+//
+//        //final TextView textView = binding.textSlideshow;
+//        //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
+//    }
+
+//    @Override
+//    public void onDestroyView() {
+//        super.onDestroyView();
+//        binding = null;
+//    }
+}
