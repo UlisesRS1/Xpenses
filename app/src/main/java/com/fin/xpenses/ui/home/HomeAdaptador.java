edd560a372d6380b808e82f7f9c6730213a4e422
UlisesRS1 <rankulimarcus@gmail.com>
2025-05-19 13:38:30 -0600

    Configure and use viewBinding

--- app/src/main/java/com/fin/xpenses/ui/home/HomeAdaptador.java
+++ app/src/main/java/com/fin/xpenses/ui/home/HomeAdaptador.java
@@ -6,12 +6,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import androidx.annotation.NonNull;
+
+import com.fin.xpenses.databinding.ListaInicioBinding;
+import com.fin.xpenses.model.Movimiento;
+
+import java.util.List;
+
 public class HomeAdaptador extends BaseAdapter {
     private static LayoutInflater inflater = null;
+    private List<Movimiento> movimientos;
     Context contexto;
+
+    public HomeAdaptador(@NonNull Context contexto, List<Movimiento> movimientos) {
+        this.contexto = contexto;
+        inflater = LayoutInflater.from(contexto);
+        this.movimientos = movimientos;
+
+
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ListaInicioBinding listaInicioBinding;
+
         return null;
     }
 
