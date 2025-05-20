d62aff119035c16c27e1e4e85af97854d2c62e5f
Angel <angelnoecg@gmail.com>
2025-05-19 19:01:53 -0600

    Diseño de la pagina acerca de 2

--- app/src/main/java/com/fin/xpenses/ui/acercade/AcercaDeFragment.java
+++ app/src/main/java/com/fin/xpenses/ui/acercade/AcercaDeFragment.java
@@ -1,29 +1,36 @@
 package com.fin.xpenses.ui.acercade;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-//import com.fin.xpenses.databinding.FragmentSlideshowBinding;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.fin.xpenses.databinding.FragmentAcercadeBinding;
 
 public class AcercaDeFragment extends Fragment {
 
-    //private FragmentSlideshowBinding binding;
+    private FragmentAcercadeBinding binding;
 
-//    public View onCreateView(@NonNull LayoutInflater inflater,
-//                             ViewGroup container, Bundle savedInstanceState) {
-//        SlideshowViewModel slideshowViewModel =
-//                new ViewModelProvider(this).get(SlideshowViewModel.class);
-//
-//        //binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-//        //View root = binding.getRoot();
-//
-//        //final TextView textView = binding.textSlideshow;
-//        //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-//        return root;
-//    }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        AcercaDeViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(AcercaDeViewModel.class);
 
-//    @Override
-//    public void onDestroyView() {
-//        super.onDestroyView();
-//        binding = null;
-//    }
+        binding = FragmentAcercadeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+//        final TextView textView = binding.textSlideshow;
+//        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
