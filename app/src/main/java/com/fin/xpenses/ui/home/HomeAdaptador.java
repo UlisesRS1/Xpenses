d30474de686483c42cdf6a97cf6454472de4239d
UlisesRS1 <rankulimarcus@gmail.com>
2025-05-20 08:52:59 -0600

    Implement calss HomeAdaptador.java

--- app/src/main/java/com/fin/xpenses/ui/home/HomeAdaptador.java
+++ app/src/main/java/com/fin/xpenses/ui/home/HomeAdaptador.java
@@ -8,29 +8,57 @@ import android.widget.BaseAdapter;
 
 import androidx.annotation.NonNull;
 
+import com.fin.xpenses.R;
+import com.fin.xpenses.data.DatabaseHelper;
 import com.fin.xpenses.databinding.ListaInicioBinding;
 import com.fin.xpenses.model.Movimiento;
+import com.fin.xpenses.repository.IMovimientoRepository;
+import com.fin.xpenses.repository.MovimientoRepository;
 
 import java.util.List;
 
 public class HomeAdaptador extends BaseAdapter {
     private static LayoutInflater inflater = null;
+    private Context contexto;
     private List<Movimiento> movimientos;
-    Context contexto;
 
     public HomeAdaptador(@NonNull Context contexto, List<Movimiento> movimientos) {
         this.contexto = contexto;
         inflater = LayoutInflater.from(contexto);
         this.movimientos = movimientos;
-
-
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ListaInicioBinding listaInicioBinding;
+        ViewHolder holder;
 
-        return null;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(contexto);
+            listaInicioBinding = ListaInicioBinding.inflate(inflater, parent, false);
+            holder = new ViewHolder(listaInicioBinding);
+            convertView = listaInicioBinding.getRoot();
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+            listaInicioBinding = holder.binding;
+        }
+
+        Movimiento movimiento = movimientos.get(position);
+        listaInicioBinding.txtCategoria.setText(movimiento.getIdMovimiento());
+        listaInicioBinding.txtDescripcion.setText("Descripcion (Test de descripcion)");
+        listaInicioBinding.txtCifra.setText(String.valueOf(movimiento.getMonto()));
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        ListaInicioBinding binding;
+
+
+        ViewHolder(ListaInicioBinding binding) {
+            this.binding = binding;
+        }
     }
 
     @Override
