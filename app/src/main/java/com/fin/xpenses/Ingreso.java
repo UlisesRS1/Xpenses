aca9c1a899767c9205564d30a6becb3ff3d18e0f
CesarE301 <cesar12345chuy11@gmail.com>
2025-05-27 19:10:28 -0600

    modificacion_ingreso_gasto

--- app/src/main/java/com/fin/xpenses/Ingreso.java
+++ app/src/main/java/com/fin/xpenses/Ingreso.java
@@ -34,6 +34,10 @@ public class Ingreso extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ingreso);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
